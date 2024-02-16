0e2125177c30aa126a8937c1fa6d58c10cb8e7c7
rezaputra <rezaputramardiansyah@gmail.com>
2024-02-17 02:32:35 +0700

    feat: Adding sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,5 @@
+function OrganizationIdPage() {
+    return <div>Enter</div>
+}
+
+export default OrganizationIdPage
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: "Tasktify",
+    description: "Collaborate, manage project and reach new productivity peaks, from high rises to home office",
+}
